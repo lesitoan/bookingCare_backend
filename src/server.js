a07c9be72ff730e99de6467ef3db73a250447487
lesitoan <lesitoan.media2gmail.com>
2023-05-03 00:04:24 +0700

    cai_dat_moi_truong

---
+++ src/server.js
@@ -0,0 +1,19 @@
+import express from "express";
+import bodyParser from "body-parser";
+import dotenv from "dotenv";
+import viewEngine from "./config/viewEngine";
+import initWebRoutes from "./route/web";
+
+const app = express();
+dotenv.config();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended : true}));
+
+viewEngine(app);
+initWebRoutes(app);
+
+const PORT = process.env.PORT || 8080;
+
+app.listen(PORT, ()=> {
+    console.log("Connect successfully !!!");
+});
